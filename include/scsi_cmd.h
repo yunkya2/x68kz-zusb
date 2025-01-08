8b45588ed60b072b2fcfe072554bc2ce5debd663
Yuichi Nakamura <y.512.nakamura@gmail.com>
2025-01-09 00:17:12 +0900

    Update scsi_cmd.h for additional command support

--- include/scsi_cmd.h
+++ include/scsi_cmd.h
@@ -29,16 +29,23 @@
 
 #define ATTR_PACKED   __attribute__ ((packed))
 
-#define SCSI_CMD_TEST_UNIT_READY          0x00
-#define SCSI_CMD_REZERO_UNIT              0x01
-#define SCSI_CMD_REQUEST_SENSE            0x03
-#define SCSI_CMD_FORMAT_UNIT              0x04
-#define SCSI_CMD_INQUIRY                  0x12
-#define SCSI_CMD_READ_FORMAT_CAPACITIES   0x23
-#define SCSI_CMD_READ_CAPACITY_10         0x25
-#define SCSI_CMD_READ_10                  0x28
-#define SCSI_CMD_WRITE_10                 0x2a
-#define SCSI_CMD_MODE_SENSE_10            0x5a
+#define SCSI_CMD_TEST_UNIT_READY                0x00
+#define SCSI_CMD_REZERO_UNIT                    0x01
+#define SCSI_CMD_REQUEST_SENSE                  0x03
+#define SCSI_CMD_FORMAT_UNIT                    0x04
+#define SCSI_CMD_REASSIGN_BLOCKS                0x07
+#define SCSI_CMD_SEEK                           0x0b
+#define SCSI_CMD_INQUIRY                        0x12
+#define SCSI_CMD_MODE_SELECT_6                  0x15
+#define SCSI_CMD_MODE_SENSE_6                   0x1a
+#define SCSI_CMD_START_STOP_UNIT                0x1b
+#define SCSI_CMD_PREVENT_ALLOW_MEDIUM_REMOVAL   0x1e
+#define SCSI_CMD_READ_FORMAT_CAPACITIES         0x23
+#define SCSI_CMD_READ_CAPACITY_10               0x25
+#define SCSI_CMD_READ_10                        0x28
+#define SCSI_CMD_WRITE_10                       0x2a
+#define SCSI_CMD_VERIFY_10                      0x2f
+#define SCSI_CMD_MODE_SENSE_10                  0x5a
 
 /*
  * SCSI command structure definition
@@ -85,9 +92,20 @@ typedef struct ATTR_PACKED scsi_request_sense_resp {
   uint8_t sense_key_specific[3];
 } scsi_request_sense_resp_t;
 
-// 0x04: FORMAT UNIT  (UFI specific)
+// 0x04: FORMAT UNIT
 
 typedef struct ATTR_PACKED scsi_format_unit {
+  uint8_t cmd_code;
+  uint8_t defect_list_format;
+  uint8_t vendor_specific;
+  uint8_t _reserved1;
+  uint8_t ffmt;
+  uint8_t control;
+} scsi_format_unit_t;
+
+// 0x04: FORMAT UNIT  (UFI specific)
+
+typedef struct ATTR_PACKED scsi_format_unit_ufi {
   uint8_t cmd_code;
   uint8_t defect_list_format;
   uint8_t track_number;
@@ -95,9 +113,9 @@ typedef struct ATTR_PACKED scsi_format_unit {
   uint8_t _reserved1[2];
   uint16_t alloc_length;
   uint8_t _reserved2[3];
-} scsi_format_unit_t;
+} scsi_format_unit_ufi_t;
 
-typedef struct ATTR_PACKED scsi_format_unit_param {
+typedef struct ATTR_PACKED scsi_format_unit_ufi_param {
   uint8_t _reserved1;
   uint8_t flag;
   uint16_t defect_list_length;
@@ -105,7 +123,26 @@ typedef struct ATTR_PACKED scsi_format_unit_param {
   uint32_t block_num;
   uint8_t _reserved2[2];
   uint16_t block_size;
-} scsi_format_unit_param_t;
+} scsi_format_unit_ufi_param_t;
+
+// 0x07: REASSIGN BLOCKS
+
+typedef struct ATTR_PACKED scsi_reassign_blocks {
+  uint8_t cmd_code;
+  uint8_t flag_1;
+  uint8_t _reserved1[3];
+  uint8_t control;
+} scsi_reassign_blocks_t;
+
+// 0x0b: SEEK
+
+typedef struct ATTR_PACKED scsi_seek {
+  uint8_t cmd_code;
+  uint8_t lba_msb;
+  uint16_t lba;
+  uint8_t _reserved2;
+  uint8_t control;
+} scsi_seek_t;
 
 // 0x12: INQUIRY
 
@@ -131,6 +168,48 @@ typedef struct ATTR_PACKED scsi_inquiry_resp {
   uint8_t product_id[16];
   uint8_t product_rev[4];
 } scsi_inquiry_resp_t;
+
+// 0x15: MODE SELECT (6)
+
+typedef struct ATTR_PACKED scsi_mode_select6 {
+  uint8_t cmd_code;
+  uint8_t flag_1;
+  uint8_t _reserved1[2];
+  uint8_t alloc_length;
+  uint8_t control;
+} scsi_mode_select6_t;
+
+// 0x1a: MODE SENSE (6)
+
+typedef struct ATTR_PACKED scsi_mode_sense6 {
+  uint8_t cmd_code;
+  uint8_t _reserved1;
+  uint8_t page_code;
+  uint8_t subpage_code;
+  uint8_t alloc_length;
+  uint8_t control;
+} scsi_mode_sense6_t;
+
+// 0x1b: START STOP UNIT
+
+typedef struct ATTR_PACKED scsi_start_stop_unit {
+  uint8_t cmd_code;
+  uint8_t immed;
+  uint8_t _reserved1;
+  uint8_t power_condition_modifier;
+  uint8_t power_condition;
+  uint8_t control;
+} scsi_start_stop_unit_t;
+
+// 0x1e: PREVENT ALLOW MEDIUM REMOVAL
+
+typedef struct ATTR_PACKED scsi_prevent_allow_medium_removal {
+  uint8_t cmd_code;
+  uint8_t lun;
+  uint8_t _reserved1[2];
+  uint8_t prevent;
+  uint8_t control;
+} scsi_prevent_allow_medium_removal_t;
 
 // 0x23: READ FORMAT CAPACITIIES  (UFI specific)
 
@@ -189,6 +268,17 @@ typedef struct ATTR_PACKED scsi_write10 {
   uint16_t block_count;
   uint8_t control;
 } scsi_write10_t;
+
+// 0x2f: VERIFY (10)
+
+typedef struct ATTR_PACKED scsi_verify10 {
+  uint8_t cmd_code;
+  uint8_t _reserved1;
+  uint32_t lba;
+  uint8_t _reserved2;
+  uint16_t block_count;
+  uint8_t control;
+} scsi_verify10_t;
 
 // 0x5a: MODE SENSE (10)
 
